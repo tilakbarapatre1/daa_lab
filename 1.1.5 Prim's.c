b16138ed993afafa58cfdb8eae56ba99800c75cc
tilakbarapatre1 <tilak.barapatre.batch2024@sitnagpur.siu.edu.in>
2025-08-04 13:56:57 +0530

    Create 1.1.5 Prim's.c

---
+++ 1.1.5 Prim's.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+void printPass(int arr[], int start, int end) {
+    printf("Pass:");
+    for (int i = start; i <= end; i++) {
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+}
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0; j = 0; k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+
+    printPass(arr, l, r);
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int n;
+    printf("no of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Given array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
